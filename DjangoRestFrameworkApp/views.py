f388bf549330631f3079204af251682609fbb114
parimalmahindrakar <parimalm4653@gmail.com>
2021-02-20 11:04:31 +0530

    added the code of mixins exmaple

--- DjangoRestFrameworkApp/views.py
+++ DjangoRestFrameworkApp/views.py
@@ -11,78 +11,25 @@ from django.utils.decorators import method_decorator
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.generics import *
+from rest_framework.mixins import *
 
-
-class EmployeeAPIView(APIView):
-
-    def get(self, request, format=None):
-        qs = Employee.objects.all()
-        serializer = EmployeeSerializer(qs, many=True)
-        return Response(serializer.data)
-
-# Only list the records
-class EmployeeListApiView(ListAPIView):
-    # queryset = Employee.objects.all()
-    serializer_class = EmployeeSerializer
-
-    def get_queryset(self):
-        qs = Employee.objects.all()
-        name = self.request.GET.get("ename")
-        if name is not None:
-            qs = qs.filter(ename__icontains=name)
-        return qs
-
-# only create
-class EmployeeCreateApiView(CreateAPIView):
-    qs = Employee.objects.all()
-    serializer_class = EmployeeSerializer
-
-# only retieve
-class EmployeeRetrieveApiView(RetrieveAPIView):
+class EmployeeListCreateModelMixin(CreateModelMixin, ListAPIView):
     queryset = Employee.objects.all()
     serializer_class = EmployeeSerializer
 
-# only update
-class EmployeeUpdateApiView(UpdateAPIView):
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+
+class EmployeeRetrieveUpdateDeleteModelMixin(UpdateModelMixin,DestroyModelMixin,RetrieveAPIView):
     queryset = Employee.objects.all()
     serializer_class = EmployeeSerializer
 
-# only delete
-class EmployeeDeleteApiView(DestroyAPIView):
-    queryset = Employee.objects.all()
-    serializer_class = EmployeeSerializer
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
 
-
-
-
-# list employee records and also create it
-class EmployeeListCreateAPIView(ListCreateAPIView):
-    queryset = Employee.objects.all()
-    serializer_class = EmployeeSerializer
-
-# list employee records and also update it
-class EmployeeRetrieveUpdateAPIView(RetrieveUpdateAPIView):
-    queryset = Employee.objects.all()
-    serializer_class = EmployeeSerializer
-
-# list employee records and also delete it
-class EmployeeRetrieveDestroyAPIView(RetrieveDestroyAPIView):
-    queryset = Employee.objects.all()
-    serializer_class = EmployeeSerializer
-
-# list employee records and also update and delete it
-class EmployeeRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
-    queryset = Employee.objects.all()
-    serializer_class = EmployeeSerializer
-
-
-######################################################  only two end points  ######################################################
-
-class EmployeeListCreateAPIView(ListCreateAPIView):
-    queryset = Employee.objects.all()
-    serializer_class = EmployeeSerializer
-
-
-class EmployeeRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
-    queryset = Employee.objects.all()
-    serializer_class = EmployeeSerializer
+    def patch(self, request, *args, **kwargs):
+        return self.partial_update(request, *args, **kwargs)
+    
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
--- DjangoRestFrameworkProject/urls.py
+++ DjangoRestFrameworkProject/urls.py
@@ -18,24 +18,9 @@ from django.urls import path, include
 from DjangoRestFrameworkApp.views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("api-rest/", include("rest_framework.urls")),
-    path("api/", EmployeeAPIView.as_view()),
-    path("list_api_view/", EmployeeListApiView.as_view()),
-    path("create_api_view/", EmployeeCreateApiView.as_view()),
-    path("retrieve_api_view/<int:pk>/", EmployeeRetrieveApiView.as_view()),
-    path("update_api_view/<int:pk>/", EmployeeUpdateApiView.as_view()),
-    path("delete_api_view/<int:pk>/", EmployeeDeleteApiView.as_view()),
 
-    # multiple at a same time
-    path("list_create_api_view/", EmployeeListCreateAPIView.as_view()),
-    path("retrieve_update_api_view/<int:pk>/", EmployeeRetrieveUpdateAPIView.as_view()),
-    path("retrieve_delete_api_view/<int:pk>/", EmployeeRetrieveDestroyAPIView.as_view()),
-    path("retrieve_update_delete_api_view/<int:pk>/", EmployeeRetrieveUpdateDestroyAPIView.as_view()),
-
-
-    ##########  only two end points  #############
-    path("only_two_end_points_retrieve_create_update_delete/", EmployeeListCreateAPIView.as_view()),
-    path("only_two_end_points_retrieve_create_update_delete/<int:pk>/", EmployeeRetrieveUpdateDestroyAPIView.as_view()),
+    path("api_dev_with_mixin/", EmployeeListCreateModelMixin.as_view()),
+    path("api_dev_with_mixin/<int:pk>/", EmployeeRetrieveUpdateDeleteModelMixin.as_view()),
 
 
 
